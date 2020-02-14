395e423829817d862999585dfce1b65a87270a93
Brian Desplantes <heftygac@gmail.com>
2020-02-14 00:25:40 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -47,9 +47,9 @@ def naming_system
    :sub_category => {
      :label => "Phylum",
      :sub_category => {
-       :label => "Class"
+       :label => "Class",
        :sub_category => {
-         :label => "Order"
+         :label => "Order",
          :sub_category => nil
        }
      }
